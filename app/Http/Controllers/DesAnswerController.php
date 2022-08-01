9152c9b3725552f017798b8dfb69134b0e64e905
pouriya <amiralibg0135@gmail.com>
2022-08-02 01:39:22 +0330

    upload

---
+++ app/Http/Controllers/DesAnswerController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DescriptiveAnswer;
+use Illuminate\Http\Request;
+use App\Http\Resources\DesAnswerResource;
+use Illuminate\Support\Facades\Validator;
+
+class DesAnswerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return DesAnswerResource::collection(DescriptiveAnswer::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+//        $validated = Validator::make($request->all(), [
+//            'question_id' => 'required',
+//            'answer' => 'required'
+//        ])->validate();
+
+        foreach($request->all() as $key => $value) {
+            $desAnswer = DescriptiveAnswer::create([
+                'user_id' => auth()->user()->id,
+                'question_id' => $key,
+                'answer' => $value
+            ]);
+        }
+
+        if($desAnswer) {
+            return new DesAnswerResource($desAnswer);
+        }else{
+            return response()->json('error');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DescriptiveAnswer  $descriptiveAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DescriptiveAnswer $descriptiveAnswer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DescriptiveAnswer  $descriptiveAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DescriptiveAnswer $descriptiveAnswer)
+    {
+        //
+    }
+
+//    /**
+//     * Update the specified resource in storage.
+//     *
+//     * @param  \Illuminate\Http\Request  $request
+//     * @param  \App\Models\DescriptiveAnswer  $descriptiveAnswer
+//     * @return \Illuminate\Http\Response
+//     */
+//    public function update(Request $request, DescriptiveAnswer $descriptiveAnswer)
+//    {
+//        //
+//    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DescriptiveAnswer  $descriptiveAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DescriptiveAnswer $descriptiveAnswer)
+    {
+        //
+    }
+}
---
+++ app/Http/Resources/DesAnswerResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DesAnswerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'type' => 'Descriptive Answer',
+            'attribute' => [
+                'user_id' => $this->user_id,
+                'question_id' => $this->question_id,
+                'answer' => $this->answer,
+            ]
+        ];
+    }
+}
